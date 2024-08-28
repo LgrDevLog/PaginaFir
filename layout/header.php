237e0b5ec993ffc588e62563108a81df220116ab
Dante Aligieri Davila Pinedo <dantealigieridavilapinedo@gmail.com>
2024-08-28 17:10:05 -0500

    Primer commit

---
+++ layout/header.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="zxx">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>I.E Francisco Izquierdo Rios</title>
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon/FIRLOGO.png" />
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!--Custom CSS-->
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <!--Plugin CSS-->
+    <link href="css/plugin.css" rel="stylesheet" type="text/css" />
+    <!--Font Awesome-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
+  </head>
+  <body>
+    <!-- Preloader -->
+    <div id="preloader">
+      <div id="status"></div>
+    </div>
+    <header class="main_header_area">
+      <div class="topbar-wrap">
+        <div class="container">
+          <div class="top-info d-flex justify-content-between align-items-center">
+            <ul class="t-address">
+              <li><i class="fas fa-phone-alt"></i> (042) 600696</li>
+              <li><i class="far fa-envelope"></i> iefranciscoizquierdorios@hotmail.com
+              </li>
+              <li><i class="fas fa-map-marker-alt"></i>Jr. Alfonso Ugarte Cdra. 7</li>
+            </ul>
+            <ul class="t-social">
+              <li>
+                <a href="https://www.facebook.com/iefir.edu.pe/?locale=es_LA"><i class="fab fa-facebook-f"></i></a>
+              </li>
+              <li>
+                <span class="ct-search-link"
+                  ><a href="#"><i class="fa fa-search"></i></a
+                ></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <!-- Navigation Bar -->
+      <div class="header_menu">
+        <nav class="navbar navbar-default">
+          <div class="container">
+            <div class="navbar-flex d-flex align-items-center justify-content-between w-100">
+              <!-- Brand and toggle get grouped for better mobile display -->
+              <div class="navbar-header">
+                <a class="navbar-brand text-center" href="index.html">
+                  <img src="images/logo/logo.img" alt="image" />
+                </a>
+              </div>
+              <!-- Collect the nav links, forms, and other content for toggling -->
+              <div class="navbar-collapse1 w-100" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav" id="responsive-menu">
+                  <li class="dropdown submenu active">
+                    <li><a href="index.php">inicio</a></li>
+          
+                  </li>
+                  <li class="dropdown submenu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"
+                      >NOSOTROS <i class="fas fa-chevron-down"></i
+                    ></a>
+                    <ul class="dropdown-menu">
+                      <li><a href="./layout/historia.php">HISTORIA DEL COLEGIO</a> 
+                    
+                    </li>
+                      <li><a >HIMNO DEL COLEGIO</a></li>
+                      <li><a >MISION Y VISION</a></li>
+                      <li><a >AUTORIDADES</a></li>
+                      <li><a >OBJETIVOS</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown submenu">
+                    <li class="dropdown submenu active">
+                      <li><a >Primaria</a></li>
+                
+                    </li>
+                  </li>
+                  <li class="dropdown submenu">
+                    <li class="dropdown submenu active">
+                      <li><a >Secundaria</a></li>
+  
+                
+                    </li>
+                  </li>
+                  <li class="dropdown submenu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"
+                      >BLOG <i class="fas fa-chevron-down"></i
+                    ></a>
+                    <ul class="dropdown-menu">
+                      <li><a >GALERIA</a></li>
+                      <li><a >EVENTOS</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown submenu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"
+                      >REGLAMENTOS <i class="fas fa-chevron-down"></i
+                    ></a>
+                    <ul class="dropdown-menu">
+                      <li><a >REGLAMENTO INTERNO</a></li>
+                      <li><a >UNIFORME</a></li>
+                    </ul>
+                  </li>
+                  <li><a >Matriculate</a></li>
+
+                  <li><a >Contactanos</a></li>
+                  <li><a >logueate</a></li>
+                  
+                </ul>
+              </div>
+
+              <div id="slicknav-mobile"></div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+  </body>
+
+   
